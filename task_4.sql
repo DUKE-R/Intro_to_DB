318e9f7d9a918dd3d62a2eb7e1550252e2c053fe
Richard <odhiamboritchy@gmail.com>
2024-10-18 12:48:06 +0300

    FinALE

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
 -- Print the full description of the books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books' AND TABLE_SCHEMA = 'alx_book_store';
+WHERE TABLE_NAME = 'BOOKS' AND TABLE_SCHEMA = 'alx_book_store';
